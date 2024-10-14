f4c1df1da6803d1f32e1bb26fac0540a62417063
niellecuevas <raniella810@gmail.com>
2024-10-14 11:15:21 +0800

    van database

--- admin_app/forms.py
+++ admin_app/forms.py
@@ -4,11 +4,9 @@ from .models import Driver
 class DriverForm(forms.ModelForm):
     class Meta:
         model = Driver
-        fields = ['name', 'license', 'van_model', 'plate_number']
+        fields = ['name', 'license']
 
     def __init__(self, *args, **kwargs):
         super(DriverForm, self).__init__(*args, **kwargs)
         # Setting default values
         self.fields['license'].initial = ''  # Set initial to empty
-        self.fields['van_model'].initial = ''
-        self.fields['plate_number'].initial = ''
--- admin_app/models.py
+++ admin_app/models.py
@@ -8,8 +8,6 @@ class Driver(models.Model):
     driver_id = models.CharField(max_length=10, unique=True, editable=False)
     name = models.CharField(max_length=100)
     license = models.CharField(max_length=50, default='N/A')  # Default value set
-    van_model = models.CharField(max_length=100, default='N/A')  # Default value set
-    plate_number = models.CharField(max_length=20, default='N/A')
 
     def save(self, *args, **kwargs):
         # Automatically generate the driver ID
@@ -18,12 +16,19 @@ class Driver(models.Model):
             self.driver_id = f"{self.DRIVER_ID_PREFIX}{count:04d}"  # DRV0001, DRV0002, etc.
         if not self.license:
             self.license = 'N/A'
-        if not self.van_model:
-            self.van_model = 'N/A'
-        if not self.plate_number:
-            self.plate_number = 'N/A'
         super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.name} - {self.driver_id}"
+    
+
+    class Van(models.Model):
+        van_id = models.AutoField(primary_key=True)  # Unique identifier for the van
+        model = models.CharField(max_length=100)      # Van model
+        plate_number = models.CharField(max_length=20)  # Plate number
+        capacity = models.IntegerField()               # Capacity of the van
+        status = models.CharField(max_length=50, default='available')  # Status (e.g., available, unavailable)
+
+    def __str__(self):
+        return f"{self.model} - {self.plate_number}"
     
