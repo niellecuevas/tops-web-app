2da681065ab8336861610c4a68bda04a0903a3d4
niellecuevas <raniella810@gmail.com>
2024-10-20 10:20:43 +0800

    fetching van details from the van card to modal

--- customer_app/views.py
+++ customer_app/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from admin_app.models import Van
 from django.shortcuts import redirect
 from .forms import Booking  # Import your form here
 
@@ -119,6 +120,9 @@ def success(request):
 
 def customer_homepage2(request):
     came_from_payment = request.GET.get('came_from_payment', 'false')  # Defaults to 'false'
+    # Fetch the van with ID 23
+    van = get_object_or_404(Van, id=23)
+    van2 = get_object_or_404(Van, id=24)
     
     context = {
         'came_from_payment': came_from_payment,
@@ -133,6 +137,8 @@ def customer_homepage2(request):
         'dropoff_address': request.session.get('dropoff_address', ''),
         'additional_notes': request.session.get('additional_notes', ''),
         'round_trip': request.session.get('round_trip', 'False'),
+        'van': van,  # Pass the van with ID 23 to the context
+        'van2': van2,
 
     }
 
