fc156af52393af021ed8d994a2734d2f0f1d7380
niellecuevas <raniella810@gmail.com>
2024-11-20 13:08:11 +0800

    localhost allowedhosts

--- tops_platform/settings.py
+++ tops_platform/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', default='')
 DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1,tops-platform.onrender.com").split(",")
 
 
 # Application definition
