5ab8a49981175cd0fd202634fc267ce3dd7edf6d
niellecuevas <raniella810@gmail.com>
2024-11-20 19:41:49 +0800

    working localhost finally

--- tops_platform/settings.py
+++ tops_platform/settings.py
@@ -22,10 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', default='')
+SECRET_KEY = os.getenv('SECRET_KEY', default='s3cr3t_k3y_for_d3v')
+
+DEBUG = True
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False").lower() == "true"
+
 
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1,tops-platform.onrender.com").split(",")
